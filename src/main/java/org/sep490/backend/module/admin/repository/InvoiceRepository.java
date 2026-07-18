6c81bb52503384159ca18aa6b0aeaaa5101f3f59
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-07-19 00:32:04 +0700

    feat: subscription for premium user

--- src/main/java/org/sep490/backend/module/admin/repository/InvoiceRepository.java
+++ src/main/java/org/sep490/backend/module/admin/repository/InvoiceRepository.java
@@ -5,6 +5,7 @@ import org.sep490.backend.module.admin.entity.enumeration.InvoiceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +15,11 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     List<Invoice> findAllByOrderByCreatedAtDesc();
     List<Invoice> findByStatusOrderByCreatedAtDesc(InvoiceStatus status);
     Optional<Invoice> findByPayosOrderCode(Long payosOrderCode);
+
+    List<Invoice> findByUser_UserIdOrderByCreatedAtDesc(Long userId);
+    Optional<Invoice> findFirstByUser_UserIdAndStatusOrderByEndDateDesc(Long userId, InvoiceStatus status);
+    boolean existsByUser_UserIdAndStatusAndEndDateAfter(Long userId, InvoiceStatus status, LocalDateTime now);
+    List<Invoice> findByUserIsNotNullAndStatusAndEndDateBefore(InvoiceStatus status, LocalDateTime now);
+    List<Invoice> findByPartnerInfoIsNotNullOrderByCreatedAtDesc();
+    List<Invoice> findByPartnerInfoIsNotNullAndStatusOrderByCreatedAtDesc(InvoiceStatus status);
 }
