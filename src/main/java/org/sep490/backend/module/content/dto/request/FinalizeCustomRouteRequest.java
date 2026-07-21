05ca675c3243279ea23e905f524353a9a4b9ede6
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-07-21 21:48:38 +0700

    refactor flow

--- src/main/java/org/sep490/backend/module/content/dto/request/FinalizeCustomRouteRequest.java
+++ src/main/java/org/sep490/backend/module/content/dto/request/FinalizeCustomRouteRequest.java
@@ -1,4 +1,12 @@
 package org.sep490.backend.module.content.dto.request;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class FinalizeCustomRouteRequest {
+    Long routeId;
+    String description;
 }
