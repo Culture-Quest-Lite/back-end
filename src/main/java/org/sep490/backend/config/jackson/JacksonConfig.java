383dccfae3ea72a4c8b96ab346186754279f4ea2
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-07-28 20:58:10 +0700

    fix: TC_CRT_011 - Round 1: valid datatype for groupName

---
+++ src/main/java/org/sep490/backend/config/jackson/JacksonConfig.java
@@ -0,0 +1,25 @@
+package org.sep490.backend.config.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
+import com.fasterxml.jackson.databind.type.LogicalType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper mapper = builder.createXmlMapper(false).build();
+
+        mapper.coercionConfigFor(LogicalType.Textual)
+                .setCoercion(CoercionInputShape.Integer, CoercionAction.Fail)
+                .setCoercion(CoercionInputShape.Float, CoercionAction.Fail)
+                .setCoercion(CoercionInputShape.Boolean, CoercionAction.Fail)
+                .setCoercion(CoercionInputShape.Object, CoercionAction.Fail);
+
+        return mapper;
+    }
+}
